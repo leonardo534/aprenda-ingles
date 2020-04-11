08ab8bd61c94804f5dc441f3f86ddbba43b2c9c3
leonardo534 <leonardoredes534@gmail.com>
2020-04-10 22:48:09 -0300

    Referencias adicionadas aos 'IDs' no activity_main.xml

--- app/src/main/java/com/leonardosilva/aprendaingles/MainActivity.java
+++ app/src/main/java/com/leonardosilva/aprendaingles/MainActivity.java
@@ -1,14 +1,23 @@
 package com.leonardosilva.aprendaingles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
     }
 }
